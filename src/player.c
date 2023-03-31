c063a85660fceacdfcdb9f6b3b794088c8fdb309
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2023-03-31 15:28:14 +0200

    Here is the begin of player.c

--- src/player.c
+++ src/player.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "player.h"
 
-// #include "player.h"
+struct player_data {
+    unsigned int id;
+    struct graph_t* graph;
+    unsigned int num_queens;
+    unsigned int* queens[NUM_PLAYERS]; 
+};
+
+static struct player_data data;
+char const *get_player_name()
+{
+    const char *bot = "BOTTES de Anton";
+    return bot;
+}
+
+void initialize(unsigned int player_id, struct graph_t* graph, unsigned int num_queens, unsigned int* queens[NUM_PLAYERS])
+{
+    data.id = player_id;
+    data.graph = graph;
+    data.num_queens = num_queens;
+    data.queens[NUM_PLAYERS] = queens[NUM_PLAYERS];
+}
