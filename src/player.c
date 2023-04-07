a2aaa1366a63d6857263868bc8838830807c8bcb
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-04-08 00:27:20 +0200

    Add a copy of the data in the client during initialization to avoid server deallocations

--- src/player.c
+++ src/player.c
@@ -7,7 +7,7 @@ struct player_data {
     unsigned int id;
     struct graph_t* graph;
     unsigned int num_queens;
-    unsigned int* queens[NUM_PLAYERS];
+    unsigned int** queens;
 };
 
 static struct player_data data;
@@ -18,10 +18,30 @@ char const* get_player_name()
     return bot;
 }
 
-void initialize(unsigned int player_id, struct graph_t *graph, unsigned int num_queens, unsigned int *queens[NUM_PLAYERS])
+void initialize(unsigned int player_id, struct graph_t* graph, unsigned int num_queens, unsigned int* queens[NUM_PLAYERS])
 {
     data.id = player_id;
-    data.graph = graph;
+
     data.num_queens = num_queens;
-    data.queens[NUM_PLAYERS] = queens[NUM_PLAYERS];
+
+    data.queens = (unsigned int**)malloc(sizeof(unsigned int*) * NUM_PLAYERS);
+    for (unsigned int p = 0; p < NUM_PLAYERS; p++) {
+        data.queens[p] = (unsigned int*)malloc(sizeof(unsigned int) * data.num_queens);
+        for (unsigned int q = 0; q < data.num_queens; q++)
+            data.queens[p][q] = queens[p][q];
+    }
+
+    data.graph = malloc(sizeof(struct graph_t));
+    data.graph->num_vertices = graph->t->size1 * graph->t->size2;
+    data.graph->t = gsl_spmatrix_uint_alloc(graph->t->size1, graph->t->size2);
+    gsl_spmatrix_uint_memcpy(data.graph->t, graph->t);
+}
+
+void finalize()
+{
+    for (unsigned int p = 0; p < NUM_PLAYERS; p++)
+        free(data.queens[p]);
+    free(data.queens);
+    gsl_spmatrix_uint_free(data.graph->t);
+    free(data.graph);
 }
