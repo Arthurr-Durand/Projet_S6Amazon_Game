6ce3721740770146098d81d7df191fdc7893e945
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-04-07 14:51:17 +0200

    added opt get for world type

--- src/server.c
+++ src/server.c
@@ -1,9 +1,12 @@
-
 #include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
-int main()
+#include "world.h"
+
+int main(int argc, char* argv[])
 {
     printf("[-] Server running");
 
@@ -17,5 +20,30 @@ int main()
 
     dlclose(handle);
 
+    int opt;
+    enum type_world w_type = SQUARED;
+    unsigned int width = 8;
+
+    while ((opt = getopt(argc, argv, "m:t:")) != 1) {
+      switch(opt) {
+      case 'm':
+	width = atoi(optarg);
+	break;
+      case 't':
+	if (strcmp(optarg, "d") == 0) {
+	  w_type = DONUT;
+	}
+	if (strcmp(optarg, "t") == 0) {
+	  w_type = CLOVER;
+	}
+	if (strcmp(optarg, "8") == 0) {
+	  w_type = EIGHT;
+	}
+	break;
+      }
+    }
+
+    struct graph_t g = {width*width, world_init(width, w_type)};
+    
     return EXIT_SUCCESS;
 }
