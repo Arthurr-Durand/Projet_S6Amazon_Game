e3b5efd0cd61f6855181a45ce23a4efb62d32eea
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-04-07 15:04:03 +0200

    started worldinit opt

--- src/world.c
+++ src/world.c
@@ -1,8 +1,20 @@
 #include "world.h"
 
-struct graph_t * world_init(int m, enum type_world t) {
-  gsl_spmatrix_uint* mat = gsl_spmatrix_uint_alloc(n, n);
-  for (each edge from i to j)
-    gsl_spmatrix_uint_set(mat, i, j, 1);
+gsl_spmatrix_uint* world_init(int m, enum type_world t) {
+  gsl_spmatrix_uint* mat;
+  switch (t) {
+  case SQUARED:
+    //mat = squared_world(m);
+    break;
+  case DONUT:
+    //mat = donut_world(m);
+    break;
+  case CLOVER:
+    //mat = clover_world(m);
+    break;
+  case EIGHT:
+    //mat = eight_world(m);
+    break;
+  }
+  return mat;
 }
-
