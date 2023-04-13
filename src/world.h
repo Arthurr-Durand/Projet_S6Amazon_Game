f9ec9cb275fe143fa6f3d820e45148e089d66528
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-04-13 11:31:37 +0200

    renamed world_init in graph_init and called world_init in server

--- src/world.h
+++ src/world.h
@@ -1,7 +1,6 @@
 #include "graph.h"
 #include <stdlib.h>
 
-
 enum type_world {
   SQUARED = 0,
   DONUT = 1,
@@ -13,18 +12,20 @@ enum sort {
   NO_SORT = 0,
   W_QUEEN = 1,
   B_QUEEN = 2,
-  ARROW = 3,
+  BLOCK = 3,
 };
 
 //
 struct world_t {
   enum sort* idx ;
-  int width;
+  unsigned int width;
 };
 
-gsl_spmatrix_uint * world_init(int m, enum type_world t);
+gsl_spmatrix_uint* graph_init(int m, enum type_world t);
 
-struct world_t * initiate_world(int width);
+gsl_spmatrix_uint* squared_graph(int m);
+
+struct world_t * world_init(int width);
 
 void free_world(struct world_t * world);
 
