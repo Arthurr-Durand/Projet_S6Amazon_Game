5a63ef33b2f0f3da5ed0d434ecab38e75d21897e
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2023-04-14 17:56:40 +0200

    THE STRAT IS OP

--- src/start.c
+++ src/start.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include "move.h"
 #include "dir.h"
+#include "graph.h"
+#include "player.h"
 
 unsigned int rand_seed = (unsigned int)time(NULL);
 
@@ -11,20 +13,70 @@ int get_rand_queen(int queen_number)
 }
 
 
-struct move_t get_rand_move(struct move_t previous_move,struct moves_t queens)
-{
-    struct move_t move = previous_move;
-    move.queen_src = previous_move.queen_dst;
-    int p = rand()%(csr->size1) ;
-    enum dir_t d = csr->p[move.queen_src]; //valeure dune case de la colonne dindice previous_move.queen_dst;
-    for(int i=0;i<rand()%(SIZE-(previous_move.queen_src%SIZE));i++){
-        for(int k=0;k<queens.current;k++){me
-            if(queens.t[k].queen_dst == )//indice ou ya marqué d )
-                return move;
-        }
-        for(int j=0;j<siz)
-        move.queen_dst = csr      ;//indice de d dans la matrice;
-    }
-    return move;
+unsigned int tiiiiiir(unsigned int pos){
 
+    while(rand()%sqrt(data.graph->num_vertices) && break != 0){
+            unsigned int k = data.graph->p[pos];
+            //for (unsigned int k = data.graph->p[ligne]; k < data.graph->p[ligne+1] ; k++)  
+                while ((data.graph->ligne[k] != j) && (break!=0)){
+                    if(k >= (data.graph->p[pos+1])){
+                        break = 0;
+                    }
+                    k++;
+                }
+                if (pas possible){
+                    break = 0;
+                }
+         
+            pos = k ;  
+        }
 }
+
+
+struct move_t get_rand_move(void)
+{
+    struct move_t next_move;
+    unsigned int ligne = data.quenns[data.id][rand()%data.num_queens];
+    next_move.queen_src= ligne;
+    unsigned int dir = data.graph->ligne[data.graph->p[ligne]];
+        while(rand()%sqrt(data.graph->num_vertices) && break != 0){
+            unsigned int k = data.graph->p[ligne];
+            //for (unsigned int k = data.graph->p[ligne]; k < data.graph->p[ligne+1] ; k++)  
+                while ((data.graph->ligne[k] != j) && (break!=0)){
+                    if(k >= (data.graph->p[ligne+1])){
+                        break = 0;
+                    }
+                    k++;
+                }
+                if (pas possible){  // depend de k
+                    break = 0;
+                }
+         
+            ligne = k ;  
+        }
+    next_move.queen_dst=ligne;
+    next_move.arrow_dst= tiiiiiir(ligne);
+}
+
+
+
+
+
+
+    // struct move_t move = previous_move;
+    // move.queen_src = previous_move.queen_dst;
+    // int p = rand()%(csr->size1) ;
+    // enum dir_t d = csr->p[move.queen_src]; //valeure dune case de la colonne dindice previous_move.queen_dst;
+    // for(int i=0;i<rand()%(SIZE-(previous_move.queen_src%SIZE));i++){
+    //     for(int k=0;k<queens.current;k++){
+    //         if(queens.t[k].queen_dst == )//indice ou ya marqué d )
+    //             return move;
+    //     }
+    //     for(int j=0;j<siz)
+    //     move.queen_dst = csr      ;//indice de d dans la matrice;
+    // }
+    // return move;
+
+    
+
+   
