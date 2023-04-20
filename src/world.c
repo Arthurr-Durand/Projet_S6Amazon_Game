3f6c909c8ba028af17a2f39098599d6156e71cd5
HAMOUCHE Luxel <Luxel.Hamouche@bordeaux-inp.fr>
2023-04-20 17:04:40 +0200

    Updated function that create the square_graph

--- src/world.c
+++ src/world.c
@@ -3,127 +3,98 @@
 
 gsl_spmatrix_uint* graph_init(int m, enum type_world t)
 {
-  gsl_spmatrix_uint* mat;
-  switch (t) {
-  case SQUARED:
-    mat = squared_graph(m);
-    break;
-  case DONUT:
-    // mat = donut_graph(m);
-    break;
-  case CLOVER:
-    // mat = clover_graph(m);
-    break;
-  case EIGHT:
-    // mat = eight_graph(m);
-    break;
-  }
-  return mat;
+    gsl_spmatrix_uint* mat;
+    switch (t) {
+    case SQUARED:
+        mat = squared_graph(m);
+        break;
+    case DONUT:
+        // mat = donut_graph(m);
+        break;
+    case CLOVER:
+        // mat = clover_graph(m);
+        break;
+    case EIGHT:
+        // mat = eight_graph(m);
+        break;
+    }
+    return mat;
 }
 
 /*
-  Créé une matrice d'adjacence de taille (m*m)*(m*m) 
+  Créé une matrice d'adjacence de taille (m*m)*(m*m)
 */
-gsl_spmatrix_uint* squared_graph(int m)
+gsl_spmatrix_uint* squared_graph(int size)
 {
-  int width = m * m;
-  gsl_spmatrix_uint* mat = gsl_spmatrix_uint_alloc(width, width);
-  for (int i = 0; i < width; i++) {
-    if (i % m == 0) {
-      if ((0 <= i) && (i <= m - 1)) {
-	gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-	gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-	gsl_spmatrix_uint_set(mat, i + m + 1, i, DIR_SE);
-      } else if ((width - m <= i) && (i <= width - 1)) {
-	gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-	gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-	gsl_spmatrix_uint_set(mat, i - m - 1, i, DIR_NE);
-      } else {
-	gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-	gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-	gsl_spmatrix_uint_set(mat, i + m + 1, i, DIR_SE);
-	gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-	gsl_spmatrix_uint_set(mat, i - m + 1, i, DIR_NE);
-      }
-    } else if (i % m == m - 1) {
-      if ((0 <= i) && (i <= m - 1)) {
-	gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-	gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-	gsl_spmatrix_uint_set(mat, i + m - 1, i, DIR_SW);
-      } else if ((width - m <= i) && (i <= width - 1)) {
-	gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-	gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-	gsl_spmatrix_uint_set(mat, i - m - 1, i, DIR_NW);
-      } else {
-	gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-	gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-	gsl_spmatrix_uint_set(mat, i + m - 1, i, DIR_SW);
-	gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-	gsl_spmatrix_uint_set(mat, i - m - 1, i, DIR_NW);
-      }
-    } else if ((0 <= i) && (i <= m - 1)) {
-      gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-      gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-      gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-      gsl_spmatrix_uint_set(mat, i + m - 1, i, DIR_SW);
-      gsl_spmatrix_uint_set(mat, i + m + 1, i, DIR_SE);
-    } else if ((width - m <= i) && (i <= width - 1)) {
-      gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-      gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-      gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-      gsl_spmatrix_uint_set(mat, i - m - 1, i, DIR_NW);
-      gsl_spmatrix_uint_set(mat, i - m + 1, i, DIR_NE);
-    } else {
-      gsl_spmatrix_uint_set(mat, i - 1, i, DIR_WEST);
-      gsl_spmatrix_uint_set(mat, i + 1, i, DIR_EAST);
-      gsl_spmatrix_uint_set(mat, i - m, i, DIR_NORTH);
-      gsl_spmatrix_uint_set(mat, i - m - 1, i, DIR_NW);
-      gsl_spmatrix_uint_set(mat, i - m + 1, i, DIR_NE);
-      gsl_spmatrix_uint_set(mat, i + m, i, DIR_SOUTH);
-      gsl_spmatrix_uint_set(mat, i + m - 1, i, DIR_SW);
-      gsl_spmatrix_uint_set(mat, i + m + 1, i, DIR_SE);
+    gsl_spmatrix_uint* mat = gsl_spmatrix_uint_alloc(size * size, size * size);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int index_i_j = i * size + j;
+            gsl_spmatrix_uint_set(mat, index_i_j, index_i_j, 0);
+            if (i > 0) {
+                gsl_spmatrix_uint_set(mat, index_i_j, (i - 1) * size + j, DIR_NORTH);
+                if (j > 0)
+                    gsl_spmatrix_uint_set(mat, index_i_j, (i - 1) * size + j - 1, DIR_NW);
+                if (j < size - 1)
+                    gsl_spmatrix_uint_set(mat, index_i_j, (i - 1) * size + j + 1, DIR_NE);
+            }
+            if (i < size - 1) {
+                gsl_spmatrix_uint_set(mat, index_i_j, (i + 1) * size + j, DIR_SOUTH);
+                if (j > 0)
+                    gsl_spmatrix_uint_set(mat, index_i_j, (i + 1) * size + j - 1, DIR_SW);
+                if (j < size - 1)
+                    gsl_spmatrix_uint_set(mat, index_i_j, (i + 1) * size + j + 1, DIR_SE);
+            }
+            if (j > 0)
+                gsl_spmatrix_uint_set(mat, index_i_j, i * size + j - 1, DIR_WEST);
+            if (j < size - 1)
+                gsl_spmatrix_uint_set(mat, index_i_j, i * size + j + 1, DIR_EAST);
+        }
     }
-  }
 
-  gsl_spmatrix_uint* csr = gsl_spmatrix_uint_compress(mat, GSL_SPMATRIX_CSR);
-  gsl_spmatrix_uint_free(mat);
+    gsl_spmatrix_uint* csr = gsl_spmatrix_uint_compress(mat, GSL_SPMATRIX_CSR);
+    gsl_spmatrix_uint_free(mat);
 
-  return csr;
+    return csr;
 }
 
 /*
   Tableau alloué dynamiquement représentant le plateau jeu qui sera modifié tout au long de la partie
 */
-struct world_t * world_init(int width)  {
-  int WORLD_SIZE = width * width;
-  struct world_t * world = malloc(sizeof(struct world_t));
-  world->width = width;
-  world->idx = malloc(sizeof(enum sort) * (WORLD_SIZE));
-  for (int i = 0; i < WORLD_SIZE; i++) {
-    world->idx[i] = 0;
-  }
-  
-  return world;
+struct world_t* world_init(int width)
+{
+    int WORLD_SIZE = width * width;
+    struct world_t* world = malloc(sizeof(struct world_t));
+    world->width = width;
+    world->idx = malloc(sizeof(enum sort) * (WORLD_SIZE));
+    for (int i = 0; i < WORLD_SIZE; i++) {
+        world->idx[i] = 0;
+    }
+
+    return world;
 }
 
 /*
-  free le tableau world alloué dynamiquement 
+  free le tableau world alloué dynamiquement
 */
-void free_world(struct world_t * world) {
-  free(world->idx);
-  free(world);
+void free_world(struct world_t* world)
+{
+    free(world->idx);
+    free(world);
 }
 
 /*
-  c'est dans le nom de la fonction XD 
+  c'est dans le nom de la fonction XD
 */
-void print_world(struct world_t * world) {
-  int size = world->width;
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      printf("%d ",world->idx[j + i*size]);
+void print_world(struct world_t* world)
+{
+    int size = world->width;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", world->idx[j + i * size]);
+        }
+        printf("\n");
     }
     printf("\n");
-  }
-  printf("\n");
 }
