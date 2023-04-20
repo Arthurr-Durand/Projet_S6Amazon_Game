c2c5ee7e1d5d22bb5ca4ed354846816dcfd405c3
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2023-04-20 10:23:23 +0200

    fonction play qui marche pas

--- src/player.c
+++ src/player.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
+#include "move.h"
+#include "dir.h"
+#include "graph.h"
 #include "player.h"
 
 struct player_data {
@@ -35,6 +38,63 @@ void initialize(unsigned int player_id, struct graph_t* graph, unsigned int num_
     data.graph->num_vertices = graph->t->size1 * graph->t->size2;
     data.graph->t = gsl_spmatrix_uint_alloc(graph->t->size1, graph->t->size2);
     gsl_spmatrix_uint_memcpy(data.graph->t, graph->t);
+}
+
+unsigned int rand_seed = (unsigned int)time(NULL);
+
+int get_rand_queen(int queen_number)
+{
+    return rand()%queen_number;    
+}
+
+
+unsigned int tiiiiiir(unsigned int pos){
+    unsigned int dir=(rand()%8)+1;
+    int stop =1;
+    while((rand()%sqrt(data.graph->num_vertices)) && (stop != 0)){
+            unsigned int k = data.graph->t[pos][0];
+            //for (unsigned int k = data.graph->p[ligne]; k < data.graph->p[ligne+1] ; k++)  
+                while ((data.graph->t[pos][k] != dir) && (stop!=0)){
+                    if(k >= (data.graph->p[pos+1])){
+                        stop = 0;
+                        k--;
+                    }
+                    k++;
+                }
+                // if (pas possible){
+                //     stop = 0;
+                // }
+            pos = k ;  
+        }
+    return pos;
+}
+
+
+struct move_t get_rand_move(void)
+{
+    struct move_t next_move;
+    unsigned int ligne = data.queens[data.id][rand()%data.num_queens];
+    next_move.queen_src= ligne;
+    unsigned int dir = data.graph->t[ligne][data.graph->t[ligne][0]]; //direction == premiere elements non nul de la ligne n:ligne
+    int stop=1;
+        while(rand()%sqrt(data.graph->num_vertices) && (stop != 0)){ //condition d'arret
+            unsigned int j = data.graph->t[ligne][0]; // on test tt les k elements de la 
+            //for (unsigned int k = data.graph->p[ligne]; k < data.graph->p[ligne+1] ; k++)  
+                while ((data.graph->t[ligne][k] != dir) && (stop!=0)){
+                    if(k >= (data.graph->t[ligne+1])){
+                        stop = 0;
+                        k--;
+                    }
+                    k++;
+                }
+                // if (pas possible){  // depend de k
+                //     stop = 0;
+                // }
+            ligne = k ;  
+        }
+    next_move.queen_dst=ligne;
+    next_move.arrow_dst= tiiiiiir(ligne);
+    return next_move;
 }
 
 void finalize()
