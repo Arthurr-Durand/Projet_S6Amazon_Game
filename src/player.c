04c4da1a5a3365073568c01c3c04521c3768a188
DURAND Arthur <Arthur.Durand@bordeaux-inp.fr>
2023-04-28 16:33:10 +0200

    Here is the "tiiir" function for the strat (commit made with Emacs (SORRY °_° )

--- src/player.c
+++ src/player.c
@@ -11,6 +11,8 @@ struct player_data {
     struct graph_t* graph;
     unsigned int num_queens;
     unsigned int** queens;
+    unsigned int* arrows;
+    unsigned int arrows_num;
 };
 
 static struct player_data data;
@@ -26,7 +28,8 @@ void initialize(unsigned int player_id, struct graph_t* graph, unsigned int num_
     data.id = player_id;
 
     data.num_queens = num_queens;
-
+    data.arrows_num = 0;
+    data.arrows = malloc(data.arrows_num * sizeof(int));
     data.queens = (unsigned int**)malloc(sizeof(unsigned int*) * NUM_PLAYERS);
     for (unsigned int p = 0; p < NUM_PLAYERS; p++) {
         data.queens[p] = (unsigned int*)malloc(sizeof(unsigned int) * data.num_queens);
@@ -48,12 +51,12 @@ void initialize(unsigned int player_id, struct graph_t* graph, unsigned int num_
 //     while((rand()%sqrt(data.graph->num_vertices)) && (stop != 0)){
 //             unsigned int k = data.graph->t[pos][0];
 //             //for (unsigned int k = data.graph->p[ligne]; k < data.graph->p[ligne+1] ; k++)
-//                 while ((data.graph->t[pos][k] != dir) && (stop!=0)){
-//                     if(k >= (data.graph->p[pos+1])){
-//                         stop = 0;
-//                         k--;
+//                 while ((memorySize dir) && (stop!=0)){
+//                     if(kmemorySize])){
+//                         memorySize
+//                         memorySize
 //                     }
-//                     k++;
+//                     k++;memorySize
 //                 }
 //                 // if (pas possible){
 //                 //     stop = 0;
@@ -103,7 +106,49 @@ unsigned int get_next_postion(unsigned int position, unsigned int dir)
         break;
     }
 
+    for (unsigned int i = 0; i < NUM_PLAYERS; i++) {
+        for (unsigned int j = 0; i < data.num_queens; i++) {
+            if (data.queens[i][j] == new_position)
+                return position;
+        }
+    }
     return new_position;
+}
+
+void shift_a_droite(int p,unsigned int* tablo, unsigned int arrow_p)
+{
+     unsigned int k = arrow_p;
+    for (unsigned int i = p; i < data.arrows_num - 1; i++) {
+        unsigned int tmp = tablo[i + 1];
+        tablo[i] = k;
+        tablo[i + 1] = tablo[i];
+        k = tmp;
+    }
+}
+
+void tablo_de_fleches(unsigned int* toblo,unsigned int pos)
+{
+    unsigned int k = 0;
+    while ((toblo[k] < pos) && (k!=data.arrows_num-1)){
+        k++;
+    }
+    shift_a_droite(k,toblo,pos);
+}
+
+int tiiir(int queen_pos)
+{
+
+    data.arrows_num++;
+    data.arrows = realloc(data.arrows, data.arrows_num * sizeof(int));
+    unsigned int arrow_pos = 0;
+    int find = 0;
+    unsigned int dir;
+    for (int k = data.graph->t->p[queen_pos]; k < data.graph->t->p[queen_pos + 1] && !find; k++) { // For each accessible directions
+        dir = gsl_spmatrix_uint_get(data.graph->t, queen_pos, data.graph->t->i[k]);
+        arrow_pos = get_next_postion(queen_pos, dir);
+    }
+    tablo_de_fleches(data.arrows, arrow_pos);
+    return arrow_pos;
 }
 
 struct move_t play(struct move_t previous_move)
@@ -113,6 +158,11 @@ struct move_t play(struct move_t previous_move)
     unsigned int queen_position;
     unsigned int new_queen_position;
     unsigned int dir;
+    for (unsigned int i = 0; i < data.num_queens; i++) {
+        unsigned int queen = data.queens[(data.id + 1) % NUM_PLAYERS][i];
+        if (queen == previous_move.queen_src)
+            queen = previous_move.queen_dst;
+    }
 
     unsigned int find = 0;
     for (unsigned int queen_nb = 0; (queen_nb < data.num_queens && !find); queen_nb++) { // For each queen
@@ -129,7 +179,7 @@ struct move_t play(struct move_t previous_move)
         }
     }
 
-    next_move.arrow_dst = 0; // TO DO
+    next_move.arrow_dst = tiiir(new_queen_position); // TO DO
 
     return next_move;
 
