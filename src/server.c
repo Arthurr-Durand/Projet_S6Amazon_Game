6a9d9800a08dfc5c878e2d2e458bf1cd9891aa05
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-05-01 00:33:55 +0200

    Improved game loop but it doesn't finish

--- src/server.c
+++ src/server.c
@@ -10,35 +10,40 @@
 #include "tools.h"
 
 enum player_color {
-  BLACK = 0,
-  WHITE = 1,
-  NB_COLORS = 2,
+    BLACK = 0,
+    WHITE = 1,
+    NB_COLORS = 2,
 };
 
 struct player {
-  char const* (*get_player_name)(void);
-  void (*initialize)(unsigned int, struct graph_t*, unsigned int, unsigned int**);
-  struct move_t (*play)(struct move_t);
-  void (*finalize)(void);
-  enum player_color color;
+    char const* (*get_player_name)(void);
+    void (*initialize)(unsigned int, struct graph_t*, unsigned int, unsigned int**);
+    struct move_t (*play)(struct move_t);
+    void (*finalize)(void);
+    enum player_color color;
 };
 
 char const* start_player(char const* player_1, char const* player_2)
 {
-  if (rand() % 2 == 0)
-    return player_1;
-  return player_2;
+    if (rand() % 2 == 0)
+        return player_1;
+    return player_2;
 }
 
 struct player* compute_next_player(struct moves_t* moves, struct player* player_1, struct player* player_2)
 {
-  if (player_1->color == moves->current)
-    return player_1;
-  return player_2;
+    if (player_1->color == (moves->current % 2))
+        return player_1;
+    return player_2;
 }
 
 enum player_color sort_to_player_color(enum sort sort) {
-  return ( sort == B_QUEEN )? BLACK : WHITE; 
+    return ( sort == B_QUEEN )? BLACK : WHITE;
+}
+
+enum sort player_color_to_sort(enum player_color color)
+{
+    return (color == BLACK ) ? B_QUEEN : W_QUEEN;
 }
 
 int main(int argc, char* argv[])
@@ -63,23 +68,23 @@ int main(int argc, char* argv[])
     
     iencly.get_player_name = dlsym(player_1, "get_player_name" );
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
     iencly.initialize = dlsym(player_1, "initialize" );
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
-    //iencly.play = dlsym(player_1, "play");
+    iencly.play = dlsym(player_1, "play");
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
     iencly.finalize = dlsym(player_1, "finalize");
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
 
     printf("[-] Client 1 successfully loaded\n\n");
@@ -94,23 +99,23 @@ int main(int argc, char* argv[])
     
     internet.get_player_name = dlsym(player_2, "get_player_name" );
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
     internet.initialize = dlsym(player_2, "initialize" );
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
-    //internet.play = dlsym(player_2, "play");
+    internet.play = dlsym(player_2, "play");
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
     internet.finalize = dlsym(player_2, "finalize");
     if ((error = dlerror()) != NULL) {
-      fputs(error, stderr);
-      exit(1);
+        fputs(error, stderr);
+        exit(1);
     }
 
     printf("[-] Client 2 successfully loaded\n\n");
@@ -147,7 +152,7 @@ int main(int argc, char* argv[])
 
     struct graph_t graph = { width * width, graph_init(width, w_type) };
     struct world_t * world = world_init(width);
-        
+    
     int num_queens = 4*(width/10 + 1);
     unsigned int* queens[NUM_PLAYERS];
     queens[0] = malloc(sizeof(unsigned int) * num_queens);
@@ -166,22 +171,27 @@ int main(int argc, char* argv[])
     struct player* current_player;
     int game_time = 15;
     int turn = 0;
-    int tries[2] = {};
-    int max_try = 5;
+    //int tries[2] = {};
+    //int max_try = 5;
     // while (???)
     //on fait un compte de tentative par tour, et on fait un compte de tentative
-    while ( (turn < game_time) && (tries[0] < max_try ) && ( tries[1] < max_try ) ){
-      current_player = compute_next_player(moves, &iencly, &internet);
-      move = current_player->play(move);    
-      if (!is_move_valid(&graph, world, moves, move) && (current_player->color != sort_to_player_color(world->idx[move.queen_src])) ){
-	tries[current_player->color]++;
-	}
-      else {
-	world->idx[move.queen_src] = NO_SORT;
-	world->idx[move.queen_dst] = current_player->color;
-	world->idx[move.arrow_dst] = BLOCK;
-      }
-      turn = turn + 1;
+    print_world(world);
+    while ( (turn < game_time) ){//&& (tries[0] < max_try ) && ( tries[1] < max_try ) ){
+        current_player = compute_next_player(moves, &iencly, &internet);
+        move = current_player->play(move);
+        //printf("valide ? %d", !is_move_valid(&graph, world, moves, move));
+        if (!is_move_valid(&graph, world, move) || (current_player->color != sort_to_player_color(world->idx[move.queen_src]))){
+	//tries[current_player->color]++;
+            printf("invalid move\n");
+	    }
+        else {
+            moves = moves_add(moves, move);
+            world->idx[move.queen_dst] = world->idx[move.queen_src];
+            world->idx[move.queen_src] = NO_SORT;
+            world->idx[move.arrow_dst] = BLOCK;
+        }
+        turn = turn + 1;
+        print_world(world);
     }
     
     free_world(world);
